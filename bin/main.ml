83e3d022fc7b1cc8c97af04088ad4b932771dae6
lmdexpr <lmdexpr@gmail.com>
2021-11-03 01:45:45 +0900

    add encode script

--- bin/main.ml
+++ bin/main.ml
@@ -1,24 +1,45 @@
-open Async
+pen Async
 open Core
 open Disml
 open Models
 
-(* Create a function to handle message_create. *)
-let check_command (message:Message.t) =
-  if String.is_prefix ~prefix:"!ping" message.content then
-    Message.reply message "Pong!" >>> ignore
+let help message =
+  let help_text   = "!help : show this help."
+  and ping_help   = "!ping : replied Pong!"
+  and encode_help = "!encode : encode ratatoskr/workspace/<name>.zip (included *.flac files) to a ratatoskr/output/<name>.mp3 file. And, delete all zip files." in
+  let summary     = String.concat ~sep:"\n" [ping_help; encode_help; help_text]
+  in Message.reply message summary >>> ignore
+
+let encode _message _args = ()
+
+let check_command drive_path (message:Message.t) =
+  let cmd, rest =
+    match String.split ~on:' ' message.content with
+    | hd::tl -> hd, tl
+    | [] -> "", []
+  in match cmd with
+    | "!ping"   -> Message.reply message "Pong!" >>> ignore
+    | "!encode" -> encode drive_path message rest
+    | "!help"   -> help message
+    | _         -> ()
+
+let setup_logger () =
+  Logs.set_reporter (Logs_fmt.reporter ());
+  Logs.set_level ~all:true (Some Logs.Debug)
 
 let main () =
-  (* Register the event handler *)
-  Client.message_create := check_command;
-  (* Pull token from env var. It is not recommended to hardcode your token. *)
-  let token = match Sys.getenv "DISCORD_TOKEN" with
-  | Some t -> t
-  | None -> failwith "No token in env"
+  setup_logger ();
+  let drive_path =
+    match Sys.getenv "DRIVE_PATH" with
+    | Some path -> path
+    | None-> failwith "No drive path in env"
   in
-  (* Start client. *)
-  Client.start token >>> ignore
+    Client.message_create := check_command drive_path;
+  let token =
+    match Sys.getenv "DISCORD_TOKEN" with
+    | Some t -> t
+    | None -> failwith "No token in env"
+  in
+    Client.start token >>> ignore
 
-let _ =
-  (* Launch the Async scheduler. You must do this for anything to work. *)
-  Scheduler.go_main ~main ()
+let _ = Scheduler.go_main ~main ()
