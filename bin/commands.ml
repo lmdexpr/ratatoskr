d1cd6889d41df1f336684778bf81a15d7bf22930
lmdexpr <lmdexpr@gmail.com>
2022-03-31 13:03:43 +0900

    oha

--- bin/commands.ml
+++ bin/commands.ml
@@ -4,9 +4,17 @@ let help_txt =
   let title       = "I'm ratatoskr running up and down the yggdrasill !" 
   and help_text   = "!help : show this help."
   and ping_help   = "!ping : reply Pong!"
+  and continue_morning_help = "ohayo! : if you greet to ratatoskr, then ratatoskr continue your greeting. e.g. when you say 'oh', ratatoskr continue 'ayo!'"
   and encode_help = "!encode : encode a ratatoskr/workspace/<name>.zip (included *.flac files) to a ratatoskr/output/<name>.mp3 file."
   in
-    String.concat ~sep:"\n" [title; ping_help; encode_help; help_text]
+    String.concat ~sep:"\n" [title; ping_help; continue_morning_help; encode_help; help_text]
+
+let morning_greeting = "ohayo!"
+let check_morning_greeting inp =
+  let open Str in
+  match split (quote inp |> regexp) morning_greeting with
+  | [ t ] -> Some t
+  | _     -> None
 
 let encode () = 
   let check_extension ext file = Filename.check_suffix file ("." ^ ext) in
@@ -40,14 +48,15 @@ let encode () =
           let count      = Array.length flacs in
           let inputs     = Array.map ~f:(fun flac -> "-i " ^ flac) flacs |> String.concat_array ~sep:" " in
           let name_mp3   = tmp_dir ^ "/" ^ name ^ ".mp3" in
-          let ffmpeg_cmd = "ffmpeg " ^ inputs ^ " -filter_complex amix=inputs=" ^ (string_of_int count) ^ ":duration=longest -ab 32k -acodec libmp3lame -f mp3 " ^ name_mp3 in
+          let ffmpeg_cmd =
+            if count > 1 then "ffmpeg " ^ inputs ^ " -filter_complex amix=inputs=" ^ (string_of_int count) ^ ":duration=longest -ab 32k -acodec libmp3lame -f mp3 " ^ name_mp3
+            else "ffmpeg " ^ inputs ^ " -ab 32k -acodec libmp3lame -f mp3 " ^ name_mp3
+          in
           let _ =
-            if count > 1 then begin
-              print_endline ffmpeg_cmd;
-              Sys.command ffmpeg_cmd |> ignore;
-              let mv = String.concat ~sep:" " ["mv"; "-f"; name_mp3; ratatoskr_dir ^ "output/" ^ name ^ ".mp3"] in Sys.command mv |> ignore;
-              Sys.command ("rm -rf " ^ zip_full_path) |> ignore
-            end;
+            print_endline ffmpeg_cmd;
+            Sys.command ffmpeg_cmd |> ignore;
+            let mv = String.concat ~sep:" " ["mv"; "-f"; name_mp3; ratatoskr_dir ^ "output/" ^ name ^ ".mp3"] in Sys.command mv |> ignore;
+            Sys.command ("rm -rf " ^ zip_full_path) |> ignore;
             Sys.command ("rm -rf " ^ tmp_dir) |> ignore
           in
             "ok!\nprocessed: " ^ file_name ^ "\nif you want to make me work more, delete processed file and re-command !encode"
--- bin/main.ml
+++ bin/main.ml
@@ -3,25 +3,33 @@ open Core
 open Disml
 open Models
 
-let check_command (message:Message.t) =
+let check_command bot_user_id (message:Message.t) =
   let reply txt  = Message.reply message txt >>> ignore
   and cmd, _rest =
     match String.split ~on:' ' message.content with
     | hd::tl -> hd, tl
     | []     -> "", []
   in
+  let `User_id writer_id = message.author.id in
+  if writer_id = bot_user_id then ()
+  else
     match cmd with
     | "!ping"   -> reply "Pong!"
     | "kawaii"  -> reply "せやろ"
     | "!encode" -> (try Commands.encode () |> reply with e -> begin reply "error!"; raise e end)
     | "!help"   -> reply Commands.help_txt
-    | _         -> ()
+    | other     -> Option.iter ~f:reply (Commands.check_morning_greeting other)
 
 let main () =
   Logs.set_reporter (Logs_fmt.reporter ());
   Logs.set_level ~all:true (Some Logs.Debug);
 
-  Client.message_create := check_command;
+  let bot_user_id =
+    match Sys.getenv "RATATOSKR_USER_ID" with
+    | Some t -> int_of_string t
+    | None -> 0
+  in
+  Client.message_create := check_command bot_user_id;
   let token =
     match Sys.getenv "RATATOSKR_DISCORD_TOKEN" with
     | Some t -> t
